4128015d43eec4704f9f449397d458b11bf2867b
lftdev <torres.lf21@gmail.com>
2024-02-14 08:44:19 -0300

    Parse numbers and booleans

---
+++ src/zargs.mjs
@@ -0,0 +1,38 @@
+function getOptionIndex (args, option) {
+  const index = args.indexOf(`-${option.shortAlias}`)
+  return index !== -1 ? index : args.indexOf(`--${option.longAlias}`)
+}
+
+function parseNumber (args, option) {
+  if (option.type !== 'number') throw new Error('Expected a numeric argument.')
+  const optionIndex = getOptionIndex(args, option)
+  return parseInt(args[optionIndex + 1])
+}
+
+function parseBoolean (args, option) {
+  if (option.type !== 'boolean') throw new Error('Expected a boolean argument.')
+  const optionIndex = getOptionIndex(args, option)
+  return optionIndex !== -1
+}
+
+export function parse (argv, options) {
+  if (argv == null) throw new Error('Expected to receive the arguments array.')
+  if (options == null || (typeof options !== 'object' && !Array.isArray(options))) throw new Error('Expected to receive the options object.')
+
+  const args = argv.slice(2)
+  if (args.length === 0) throw new Error('Expected arguments array not to be empty.')
+  if (args.some(element => typeof element !== 'string')) throw new Error('Expected argv array to contain strings only.')
+
+  return options.reduce((result, current) => {
+    let parsed
+    switch (current.type) {
+      case 'number':
+        parsed = parseNumber(args, options[0])
+        break
+      case 'boolean':
+        parsed = parseBoolean(args, options[0])
+    }
+    result[current.longAlias] = parsed
+    return result
+  }, {})
+}
---
+++ tests/zargs.test.mjs
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest'
+import { parse } from '../src/zargs.mjs'
+
+describe('parse function:', () => {
+  it('should throw if argv array is not provided;', () => expect(() => parse()).toThrow())
+  it('should throw if argv array is null;', () => expect(() => parse()).toThrow())
+  it('should throw if argv array length is 2;', () => expect(() => parse(['', ''])).toThrow())
+  it('should throw if argv array does not cotain strings only;', () => expect(() => parse([2, 'df', true], {})).toThrow())
+  it('should throw if options is not an object;', () => expect(() => parse([''], [])).toThrow())
+  it('should throw if options object is not provided;', () => expect(() => parse([''])).toThrow())
+  it('should return parsed number into an object when a numeric option is given;', () => {
+    const parsed = parse(['env', 'path', '-f', '2'], [{ shortAlias: 'f', longAlias: 'foo', type: 'number' }])
+    expect(parsed.foo).toBe(2)
+  })
+  it('should return parsed boolean into an object when a boolean option is given;', () => {
+    const parsed = parse(['env', 'path', '-f'], [{ shortAlias: 'f', longAlias: 'foo', type: 'boolean' }])
+    expect(parsed.foo).toBe(true)
+  })
+})
