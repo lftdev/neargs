c576075cbfc191292923b4ef63b48eb034c27c29
lftdev <torres.lf21@gmail.com>
2024-02-21 10:53:25 -0300

    Quick fix

--- src/zargs.mjs
+++ src/zargs.mjs
@@ -2,8 +2,8 @@ import { ARRAY_OPTION_TYPE, BOOLEAN_OPTION_TYPE, NUMBER_OPTION_TYPE } from './co
 import { parseArray, parseBoolean, parseNumber } from './parse-data-type.mjs'
 
 export function parse (argv, options) {
-  if (argv == null) throw new Error('Expected to receive the arguments array.')
-  if (options == null || (typeof options !== 'object' && !Array.isArray(options))) throw new Error('Expected to receive the options object.')
+  if (!Array.isArray(argv)) throw new Error('Expected to receive the arguments array.')
+  if (!Array.isArray(options)) throw new Error('Expected to receive the options object.')
 
   const args = argv.slice(2)
   if (args.length === 0) throw new Error('Expected arguments array not to be empty.')
